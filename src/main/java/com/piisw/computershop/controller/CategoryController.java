f02cb70ae53023953b27bab3e266c588979a6d7c
Hubert Kościelski <228172@student.pwr.edu.pl>
2019-10-17 12:27:09 +0200

    Added CrossOrigin to controllers

--- src/main/java/com/piisw/computershop/controller/CategoryController.java
+++ src/main/java/com/piisw/computershop/controller/CategoryController.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 @RequestMapping("/api/products")
 public class CategoryController {
 
